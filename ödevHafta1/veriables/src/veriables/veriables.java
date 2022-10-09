1b0034787362157e99b985185e7d3905b9e2285f
Rumeysa KAŞCIOĞLU <102765303+rumeysakasc@users.noreply.github.com>
2022-10-09 14:51:11 +0300

    Add files via upload

---
+++ ödevHafta1/veriables/src/veriables/veriables.java
@@ -0,0 +1,20 @@
+package veriables;
+
+public class Veriables {
+
+	public static void main(String[] args) {
+		//'reusability' tekrar tekrar kullanılabilirlik
+		int ogrenciSayisi=10;
+		String  mesaj="Öğrenci Sayısı:";
+		System.out.println(mesaj +ogrenciSayisi);
+		System.out.println(mesaj +ogrenciSayisi);
+		System.out.println(mesaj +ogrenciSayisi);
+		System.out.println(mesaj +ogrenciSayisi);
+		
+		//Değişkenleri biz kod yazarken her sayfada tekrar tekrar değiştirmek yerine tek bir yerde değiştirme
+		//kolaylığı sağlar. Örneğin öğrenci sayısını değişken olmadan da yazabiliyorken bu veriyi değiştirmek
+		//istediğimizde tek tek kodlar üzerinde düzeltme yapmamız gerekecek buna gerek olmaması içinde
+		// değişkenleri kullanıyoruz.
+	}
+
+}
